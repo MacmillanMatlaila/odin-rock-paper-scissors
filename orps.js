107562662be6d20bb6a8fc08b9a5ebab2001c7e7
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2024-09-14 17:23:01 +0200

    Odin rock paper scissors

--- orps.js
+++ orps.js
@@ -1,29 +1,37 @@
 getHumanChoice = window.prompt("what is your choice:", "ROCK, PAPER OR SCISSORS");
-const stringRandom = ["rock","paper","scissors"];
+const options = ["rock","paper","scissors"];
 
 function getComputerChoice(rock, paper, scissors) {
-    const options = stringRandom[Math.floor(Math.random()*stringRandom.length)];
+    const choice = options[Math.floor(Math.random()*options.length)];
 
-    return options;
+    ;
 }
 
+function checkWinner(playerSelection, computerSelection){
+  if(playerSelection == computerSelection){
+    return "Tie";}
+     
+  else if((playerSelection == ""rock" && computerSelection == "scissor")||
+    (playerSelection == "scissor" && computerSelection == "paper")||
+    (playerSelection == "paper" && computerSelection == "rock")
+    
+    return "Player";
 
-
-
-
-
-    /*humanScore = 0;console.log(stringRandom)
-computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-  if(humanChoice = r, p, s){
-    return(Random.computerChoice)
+    else{return "Computer";}
   }
+
+function playRound(playerSelection, computerSelection){
+  const result = checkWinner(playerSelection, computerSelection);
+ if (result == "Tie"){
+    return "Its a Tie!"
+  }
+  else if(result == "Player"){
+    return 'You win! $ {playerSelection} beats ${computerSelection}'
+  }
+else{
+  return 'You Lose! ${computerSelection} beats $ {playerSelection}'
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+}
 
-playRound(humanSelection, computerSelection);
 
-Math.random().toString("Rock").substring("Paper", "Scissors");*/
