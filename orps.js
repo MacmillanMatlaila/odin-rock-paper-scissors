8f99197826d7ec935c890a9fbcb4dce35d14664b
MacMillan Matlaila <83666081+MacmillanMatlaila@users.noreply.github.com>
2024-09-16 10:43:38 +0200

    function getComputerChoice

--- orps.js
+++ orps.js
@@ -1,19 +1,19 @@
 getHumanChoice = window.prompt("what is your choice:", "ROCK, PAPER OR SCISSORS");
 const options = ["rock","paper","scissors"];
 
-function getComputerChoice(rock, paper, scissors) {
+function getComputerChoice() {
     const choice = options[Math.floor(Math.random()*options.length)];
 
-    ;
+    
 }
 
-function checkWinner(playerSelection, computerSelection){
+/*function checkWinner(playerSelection, computerSelection){
   if(playerSelection == computerSelection){
     return "Tie";}
      
-  else if((playerSelection == ""rock" && computerSelection == "scissor")||
-    (playerSelection == "scissor" && computerSelection == "paper")||
-    (playerSelection == "paper" && computerSelection == "rock")
+  else if((playerSelection == "rock" && computerSelection == "scissors")||
+    (playerSelection == "scissors" && computerSelection == "paper")||
+    (playerSelection == "paper" && computerSelection == "rock"))
     
     return "Player";
 
@@ -33,5 +33,4 @@ else{
 }
 
 }
-
-
+*/
